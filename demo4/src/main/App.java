153a377d6f3c7c62878257a510d9ca01aa315d2f
daisuke0412 <ksmtdsk.tennis@icloud.com>
2025-04-29 11:17:44 +0900

    環境構築手順作成のため

--- demo4/src/main/App.java
+++ demo4/src/main/App.java
@@ -6,8 +6,10 @@ import java.util.List;
 import com.opencsv.CSVReader;
 
 public class App {
+	private static String csvPath = "C:\\pleiades\\2023-12\\workspace\\demo4\\src\\resources\\student_scores.csv";
+	
 	public static void main(String[] args) {
-        try (CSVReader reader = new CSVReader(new FileReader("C:\\work\\cicd_demo\\resource\\student_scores.csv"))) {
+        try (CSVReader reader = new CSVReader(new FileReader(csvPath))) {
             List<String[]> records = reader.readAll();
             for (String[] record : records) {
                 for (String field : record) {
